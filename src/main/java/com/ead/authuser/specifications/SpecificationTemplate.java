e8da80a7aad09bb6318216f0a37de4f854c48a34
layokaminski <layokaminski@gmail.com>
2023-08-18 15:38:00 -0300

    feat: Adiciona resolver config para argumentos

---
+++ src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -0,0 +1,18 @@
+package com.ead.authuser.specifications;
+
+import com.ead.authuser.models.UserModel;
+import net.kaczmarzyk.spring.data.jpa.domain.EqualIgnoreCase;
+import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SpecificationTemplate {
+
+    @And({
+            @Spec(path = "userType", spec = EqualIgnoreCase.class),
+            @Spec(path = "userStatus", spec = EqualIgnoreCase.class),
+            @Spec(path = "email", spec = LikeIgnoreCase.class)
+    })
+    public interface UserSpec extends Specification<UserModel> {}
+}
