1b755db1d0aa9dbaeae28aba8feb07891460440a
layokaminski <layokaminski@gmail.com>
2023-09-05 19:05:48 -0300

    feat: Implementa delete da relação entre user e course

--- src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
@@ -14,4 +14,8 @@ public interface UserCourseRepository extends JpaRepository<UserCourseModel, UUI
 
     @Query(value="SELECT * FROM users_courses WHERE user_user_id = :userId", nativeQuery = true)
     List<UserCourseModel> findAllUserCourseIntoUser(@Param("userId") UUID userId);
+
+    boolean existsByCourseId(UUID courseId);
+
+    void deleteAllByCourseId(UUID courseId);
 }
