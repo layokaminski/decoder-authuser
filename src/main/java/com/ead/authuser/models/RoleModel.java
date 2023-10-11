e969286a313d5f5ea07dd615b4b336d687375147
layokaminski <layokaminski@gmail.com>
2023-10-11 18:32:00 -0300

    feat: Adiciona configurações iniciais de autenticação

---
+++ src/main/java/com/ead/authuser/models/RoleModel.java
@@ -0,0 +1,38 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.RoleType;
+import com.ead.authuser.enums.UserStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "roles")
+public class RoleModel  implements GrantedAuthority, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID roleId;
+
+    @Column(nullable = false, unique = true, length = 30)
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Override
+    @JsonIgnore
+    public String getAuthority() {
+        return this.roleType.toString();
+    }
+}
