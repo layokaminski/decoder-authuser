879d5a2c02020f71785ba7e3eb527d3e31029fee
layokaminski <layokaminski@gmail.com>
2023-11-22 19:18:54 -0300

    refactor: Adiciona autorização e validação de roles para adicionar usuario como instrutor

--- src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,13 +1,17 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.DTOs.InstructorDTO;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,6 +28,10 @@ public class InstructorController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleService roleService;
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(
             @RequestBody @Valid InstructorDTO instructorDTO
@@ -34,7 +42,11 @@ public class InstructorController {
         } else {
             var userModel = userModelOptional.get();
 
+            RoleModel roleModel = roleService.findByRoleType(RoleType.ROLE_INSTRUCTOR)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is Not Found."));
+
             userModel.setUserType(UserType.INSTRUCTOR);
+            userModel.getRoles().add(roleModel);
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC-3")));
 
             userService.updateUser(userModel);
